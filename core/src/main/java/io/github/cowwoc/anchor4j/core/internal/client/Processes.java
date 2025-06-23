56fb38ded1a6d6f563b2f1e2fdbb9ae9c059202f
Gili Tzabari <cowwoc2020@gmail.com>
2025-06-23 15:18:25 -0400

    Removed unused method

--- core/src/main/java/io/github/cowwoc/anchor4j/core/internal/client/Processes.java
+++ core/src/main/java/io/github/cowwoc/anchor4j/core/internal/client/Processes.java
@@ -1,7 +1,6 @@
 package io.github.cowwoc.anchor4j.core.internal.client;
 
 import io.github.cowwoc.pouch.core.WrappedCheckedException;
-import org.slf4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -41,32 +40,6 @@ public final class Processes
 		 * @throws IOException if the command fails
 		 */
 		void run() throws IOException, InterruptedException;
-	}
-
-	/**
-	 * Discards the remaining data from the given reader and closes it.
-	 * <p>
-	 * This is typically used to drain a process's output or error reade to avoid deadlocks when the process
-	 * writes more data than the reader's buffer can hold.
-	 *
-	 * @param reader the reader
-	 * @param log    the logger used to record any exceptions that are thrown while reading from the reader
-	 */
-	public static void discard(BufferedReader reader, Logger log)
-	{
-		try (reader)
-		{
-			while (true)
-			{
-				String line = reader.readLine();
-				if (line == null)
-					break;
-			}
-		}
-		catch (IOException | RuntimeException e)
-		{
-			log.error("", e);
-		}
 	}
 
 	/**
